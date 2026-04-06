64f5b371c5633630659db1a3925a66b4844568af
HenryDavid Cohen <henrydavidmcohen@gmail.com>
2026-04-06 16:03:59 -0400

    dummy values for testing purposes

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,3 +2,8 @@ import sys
 import os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../function"))
+
+os.environ.setdefault("AWS_DEFAULT_REGION", "us-east-1")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "testing")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "testing")
+os.environ.setdefault("TABLE_NAME", "visitor-count")
